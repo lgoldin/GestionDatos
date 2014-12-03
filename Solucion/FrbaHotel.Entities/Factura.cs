fddf59443eaaac3430200acf195f0f2816d6f8f9
denzai <denuzai@gmail.com>
2014-12-02 23:03:20 -0300

    avances facturacion

---
+++ Solucion/FrbaHotel.Entities/Factura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaHotel.Entities
+{
+    public class Factura
+    {
+        public long Numero { get; set; }
+
+        public DateTime Fecha { get; set; }
+        
+        public decimal Total { get; set; }
+        
+        public int EstadiaId { get; set; }
+        
+        public int TipoPagoId { get; set; }
+        
+        public List<FacturaItem> Items {get; set;} 
+    }
+}
---
+++ Solucion/FrbaHotel.Services/FacturaTipoPagoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaHotel.Services.Interfaces;
+using FrbaHotel.Repositories;
+using FrbaHotel.Entities;
+
+namespace FrbaHotel.Services
+{
+    public class FacturaTipoPagoService : IFacturaTipoPagoService
+    {
+        public IEnumerable<FacturaTipoPago> GetAll()
+        {
+            var tiposDePago = new List<FacturaTipoPago>();
+            tiposDePago.Add(new FacturaTipoPago { Id = 0, Descripcion = "- No Especificado -" });
+
+            var repository = new FacturaTipoPagoRepository();
+            repository.GetAll().ToList().ForEach(tiposDePago.Add);
+            
+            return tiposDePago;
+        }
+    }
+}
