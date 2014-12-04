df5ec942e373ceddbed5a84895d945134ff3d3c8
denzai <denuzai@gmail.com>
2014-12-03 23:58:03 -0300

    factura guarda cliente de reserva

--- Solucion/FrbaHotel/Registrar Estadia/RegistrarEstadia.cs
+++ Solucion/FrbaHotel/Registrar Estadia/RegistrarEstadia.cs
@@ -150,7 +150,7 @@ namespace FrbaHotel.Registrar_Estadia
 
         private void GoToFacturacion(int estadiaId)
         {
-            Form form = new Facturacion.Facturacion(estadiaId);
+            Form form = new Facturacion.Facturacion(estadiaId, this.Reserva.ClienteId);
             DisplayForm(form);
         }
 
