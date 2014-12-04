9a1385c34a01401e4dfbb34b9dca87ed10b90f33
Gabriel Gartensztern <gabigartensz@gmail.com>
2014-12-04 02:25:59 -0300

    Listado

---
+++ Solucion/FrbaHotel.Services/Interfaces/IListadoEstadisticoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace FrbaHotel.Services.Interfaces
+{
+    public interface IListadoEstadisticoService
+    {
+        DataTable Get(string spName, DateTime fechaDesde, DateTime fechaHasta);
+    }
+}
---
+++ Solucion/FrbaHotel.Services/ListadoEstadisticoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaHotel.Services.Interfaces;
+using System.Data;
+using FrbaHotel.Repositories;
+
+namespace FrbaHotel.Services
+{
+    public class ListadoEstadisticoService : IListadoEstadisticoService
+    {
+        public DataTable Get(string spName, DateTime fechaDesde, DateTime fechaHasta)
+        {
+            return new ListadoEstadisticoRepository().Get(spName, fechaDesde, fechaHasta);
+        }
+    }
+}
